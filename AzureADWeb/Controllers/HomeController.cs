c10354170f928407a578a87538736c6e7f3ec8ed
Sameer Bhat <sameerbhat76@gmail.com>
2023-04-14 10:42:51 +0100

    added .git ignore

--- AzureADWeb/Controllers/HomeController.cs
+++ AzureADWeb/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AzureADWeb.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AzureADWeb.Controllers;
 
@@ -18,7 +19,7 @@ public class HomeController : Controller
         return View();
     }
 
-    
+    [Authorize]
     public IActionResult Privacy()
     {
         return View();
